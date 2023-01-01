f648500ffc31b8c3cb0d2b3cd889df57f91906ec
Christoph Langen <christophlangen24@gmail.com>
2023-01-01 14:31:18 +0100

    add basic html structure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+        <img src="logo.png" alt="Little Lemon logo" />
+        <h1>My App</h1>
+    </header>
+  );
+}
+
+export default Header;
